dbea6d5f2563f4b7473917bc0981b2451da3d2be
upamune <jajkeqos@gmail.com>
2015-11-21 13:46:09 +0900

    :white_check_mark: add team test

--- esa/team_test.go
+++ esa/team_test.go
@@ -1,1 +1,53 @@
-package esa_test
+package esa
+import (
+	"testing"
+	"reflect"
+	"fmt"
+)
+
+
+func TestTeamGetTeams(t *testing.T) {
+	type TestCase struct {
+		in string
+		out TeamsResponse
+	}
+
+	testCase := TestCase{
+		in: "../tests/stubs/team_teams.json",
+	}
+
+	serve, client := Stub(testCase.in, &testCase.out)
+	defer serve.Close()
+
+	res, err := client.Team.GetTeams()
+	if err != nil {
+		t.Errorf("error Request %s\n", err)
+	}
+
+	if !reflect.DeepEqual(*res, testCase.out) {
+		t.Errorf("error Response %s != %s", res, testCase.out)
+	}
+}
+
+func TestTeamGetTeam(t *testing.T) {
+	type TestCase struct {
+		in string
+		out TeamResponse
+	}
+
+	testCase := TestCase{
+		in: "../tests/stubs/team_team.json",
+	}
+
+	serve, client := Stub(testCase.in, &testCase.out)
+	defer serve.Close()
+
+	res, err := client.Team.GetTeam("docs")
+	if err != nil {
+		t.Errorf("error Request %s\n", err)
+	}
+
+	if !reflect.DeepEqual(*res, testCase.out) {
+		t.Errorf("error Response %s != %s", res, testCase.out)
+	}
+}
