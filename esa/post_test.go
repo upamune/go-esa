5a55b5b06061e66207369439d50673dd99816b5f
upamune <jajkeqos@gmail.com>
2015-11-21 14:33:57 +0900

    :white_check_mark: add post test

---
+++ esa/post_test.go
@@ -0,0 +1,145 @@
+package esa
+
+import (
+	"testing"
+	"reflect"
+	"net/url"
+	"io/ioutil"
+	"log"
+	"encoding/json"
+)
+
+
+func TestPostGetTeamPosts(t *testing.T) {
+	type TestCase struct {
+		in string
+		out PostsResponse
+	}
+
+	testCase := TestCase{
+		in: "../tests/stubs/post_posts.json",
+	}
+
+	serve, client := Stub(testCase.in, &testCase.out)
+	defer serve.Close()
+
+	res, err := client.Post.GetTeamPosts("docs", url.Values{})
+	if err != nil {
+		t.Errorf("error Request %s\n", err)
+	}
+
+	if !reflect.DeepEqual(*res, testCase.out) {
+		t.Errorf("error Response %s != %s", res, testCase.out)
+	}
+}
+
+func TestPostGetTeamPost(t *testing.T) {
+	type TestCase struct {
+		in string
+		out PostResponse
+	}
+
+	testCase := TestCase{
+		in: "../tests/stubs/post_post.json",
+	}
+
+	serve, client := Stub(testCase.in, &testCase.out)
+	defer serve.Close()
+
+	res, err := client.Post.GetTeamPost("docs", 1)
+	if err != nil {
+		t.Errorf("error Request %s\n", err)
+	}
+
+	if !reflect.DeepEqual(*res, testCase.out) {
+		t.Errorf("error Response %s != %s", res, testCase.out)
+	}
+}
+
+func TestPostPostTeamPost(t *testing.T) {
+	type TestCase struct {
+		in string
+		out PostResponse
+	}
+
+	testCase := TestCase{
+		in: "../tests/stubs/post_post_request_response.json",
+	}
+
+	var post Post
+	fileName := "../tests/stubs/post_post_request.json"
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := json.Unmarshal(data, &post); err != nil {
+		log.Fatalln(err)
+	}
+
+	serve, client := Stub(testCase.in, &testCase.out)
+	defer serve.Close()
+
+	res, err := client.Post.PostTeamPost("docs", post)
+
+	if err != nil {
+		t.Errorf("error Request %s\n", err)
+	}
+
+	if !reflect.DeepEqual(*res, testCase.out) {
+		t.Errorf("error Response %s != %s", res, testCase.out)
+	}
+}
+
+func TestPostPatchTeamPost(t *testing.T) {
+	type TestCase struct {
+		in string
+		out PostResponse
+	}
+
+	testCase := TestCase{
+		in: "../tests/stubs/post_patch_response.json",
+	}
+
+	var post Post
+	fileName := "../tests/stubs/post_patch_request.json"
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := json.Unmarshal(data, &post); err != nil {
+		log.Fatalln(err)
+	}
+
+	serve, client := Stub(testCase.in, &testCase.out)
+	defer serve.Close()
+
+	res, err := client.Post.PatchTeamPost("docs", 5, post)
+
+	if err != nil {
+		t.Errorf("error Request %s\n", err)
+	}
+
+	if !reflect.DeepEqual(*res, testCase.out) {
+		t.Errorf("error Response %s != %s", res, testCase.out)
+	}
+}
+
+func TestPostDeletePost(t *testing.T) {
+	type TestCase struct {
+		in string
+		out interface{}
+	}
+
+	testCase := TestCase{
+		in: "../tests/stubs/post_delete.json",
+	}
+
+	serve, client := Stub(testCase.in, &testCase.out)
+	defer serve.Close()
+
+	err := client.Post.DeleteTeamPost("docs", 5)
+
+	if err != nil {
+		t.Errorf("error Request %s\n", err)
+	}
+}
