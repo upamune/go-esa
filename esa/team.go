48fdeb5a38131f9d6090f7614551a3dca98bd898
upamune <jajkeqos@gmail.com>
2015-11-21 18:01:55 +0900

    :memo: write team doc

--- esa/team.go
+++ esa/team.go
@@ -3,13 +3,16 @@ package esa
 import "net/url"
 
 const (
+	// TeamURL esa API のチ-ムのベ-スURL
 	TeamURL = "/v1/teams"
 )
 
+// TeamService API docs: https://docs.esa.io/posts/102#4-0-0
 type TeamService struct {
 	client *Client
 }
 
+// TeamResponse チ-ムのレスポンス
 type TeamResponse struct {
 	Description string `json:"description"`
 	Icon        string `json:"icon"`
@@ -18,6 +21,7 @@ type TeamResponse struct {
 	URL         string `json:"url"`
 }
 
+// TeamsRespons 複数チ-ムのレスポンス
 type TeamsResponse struct {
 	Teams      []TeamResponse `json:"teams"`
 	PrevPage   interface{}    `json:"prev_page"`
@@ -25,6 +29,7 @@ type TeamsResponse struct {
 	TotalCount int            `json:"total_count"`
 }
 
+// GetTeams チ-ムを取得する
 func (t *TeamService) GetTeams() (*TeamsResponse, error) {
 	var teamsRes TeamsResponse
 	res, err := t.client.get(TeamURL, url.Values{}, &teamsRes)
@@ -37,6 +42,7 @@ func (t *TeamService) GetTeams() (*TeamsResponse, error) {
 	return &teamsRes, nil
 }
 
+// GetTeam チ-ム名を取得してチ-ムを取得する
 func (t *TeamService) GetTeam(teamName string) (*TeamResponse, error) {
 	var teamRes TeamResponse
 	teamURL := TeamURL + "/" + teamName
